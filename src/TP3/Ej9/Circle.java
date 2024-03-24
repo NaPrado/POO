ceddb40adb607774d39dec24aa823b32a755642f
Nahuel Prado <141145234+NaPrado@users.noreply.github.com>
2024-03-24 14:45:45 -0300

    Guía 3 Terminada

---
+++ src/TP3/Ej9/Circle.java
@@ -0,0 +1,12 @@
+package TP3.Ej9;
+
+public class Circle extends Ellipse{
+    Circle(Point center, double radius){
+        super(center,2*radius,2*radius);
+    }
+
+    @Override
+    public String toString() {
+        return "Círculo [Centro: {%.2f , %.2f} , Radio: %.2f}]".formatted(center.getX(),center.getY(),sMayorAxis/2);
+    }
+}
---
+++ src/TP3/Ej9/Figure.java
@@ -0,0 +1,8 @@
+package TP3.Ej9;
+
+public abstract class Figure {
+    public abstract double area();
+    public abstract double perimeter();
+    @Override
+    public abstract String toString();
+}
